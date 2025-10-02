d650995ed1b9165d2f4e668d7c49282fe475dc3d
Evan Morris <evandietzmorris@gmail.com>
2025-10-02 00:20:00 -0400

    clean up

--- src/adding_datasets_to_kg/util.py
+++ src/adding_datasets_to_kg/util.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 
 def get_data_directory_path():
+    output_dir = Path(__file__).parent.parent.parent / "data"
+    output_dir.mkdir(exist_ok=True)
+    return str(output_dir)
+
+def get_data_output_directory_path():
     output_dir = Path(__file__).parent.parent.parent / "data_output"
     output_dir.mkdir(exist_ok=True)
     return str(output_dir)
